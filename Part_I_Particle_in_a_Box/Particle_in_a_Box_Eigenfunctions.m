053d21059124f419f1be9dcce7f5637866417f8a
MaxButterworth <126078250+MaxButterworth@users.noreply.github.com>
2025-07-14 12:24:07 +0100

    Initial commit.

---
+++ Part_I_Particle_in_a_Box/Particle_in_a_Box_Eigenfunctions.m
@@ -0,0 +1,51 @@
+%%%%% Preamble %%%%%
+% Part I - Particle in a Box Eigenfunction Determination
+% Author: Max L Butterworth
+% MSc in Theoretical and Computational Chemistry Project
+% University of Oxford
+
+%%%%%%%%%%
+
+%%%%% Define constants %%%%%
+
+L = 1e-10 % Length of the 1D box in m
+m = 9.11e-31 % Mass of electron in kg
+h = 6.626e-34 % Planck's constant in Js
+hbar = h/(2*pi) % Definition of h bar
+N = 1000 % Number of discretisation points
+
+%%%%% Discretise the spatial domain, x %%%%%
+
+x = linspace(0, L, N) % Define the domain of the infinite potential well
+dx = x(2) - x(1); % Calculate the spatial step size
+
+%%%%% Solve the Schrödinger equation using the finite difference method %%%%%
+
+% Construct the Hamiltonian inside the infinite potential well
+laplacian = (1/dx^2) * spdiags([1, -2, 1], -1:1, N, N) % Define the Laplacian operator
+H = -((hbar^2)/(2*m)) * laplacian % Define the Hamiltonian operator
+
+% Find the eigenvalues and eigenvectors of the Hamiltonian matrix
+[psi, E] = eigs(H, 100, 'smallestabs')
+
+% Normalise eigenvectors
+psi_norm = psi/sqrt(trapz(x, abs(psi).^2))
+
+%%%%% Plot the first four eigenfunctions %%%%%
+figure % Generate a figure
+
+subplot(2, 2, 1) % Top left plot
+scatter(x, psi(:,1)) % Plot the ground state wavefunction
+title('Ground State Eigenfunction')
+
+subplot(2, 2, 2) % Top right plot
+scatter(x, psi(:,2)) % Plot the first excited state wavefunction
+title('First Excited State Eigenfunction')
+
+subplot(2, 2, 3) % Bottom left plot
+scatter(x, psi(:,3)) % Plot the second excited state wavefunction
+title('Second Excited State Eigenfunction')
+
+subplot(2, 2, 4) % Bottom right plot
+scatter(x, psi(:,4)) % Plot the third excited state wavefunction
+title('Third Excited State Eigenfunction')
