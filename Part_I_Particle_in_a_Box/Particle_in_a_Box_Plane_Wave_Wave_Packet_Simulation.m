12d0b45ab733e749c40341d9a68df1e425b13c82
MaxButterworth <126078250+MaxButterworth@users.noreply.github.com>
2025-07-15 22:04:32 +0100

    Set axes limits for plots.

--- Part_I_Particle_in_a_Box/Particle_in_a_Box_Plane_Wave_Wave_Packet_Simulation.m
+++ Part_I_Particle_in_a_Box/Particle_in_a_Box_Plane_Wave_Wave_Packet_Simulation.m
@@ -80,6 +80,7 @@ subplot(1, 3, 1) % Left subfigure
 real_wavefunction = plot(x_ang, real(psi_t(:, 1))); % Plot the real wavefunction
 xlabel('$x\ (\AA)$', 'Interpreter','latex'); % Label the x-axis
 ylabel('$\mathrm{Re}(\psi(x, t))$', 'Interpreter','latex'); % Label the y-axis
+ylim([min(real(psi_t(:))) max(real(psi_t(:)))]); % Set the y-limits foe convenience
 title('Real Component of the Wavefunction') % Add a title
 grid on; % Add a grid to the plot
 
@@ -87,6 +88,7 @@ subplot(1, 3, 2) % Middle subfigure
 imag_wavefunction = plot(x_ang, imag(psi_t(:, 1))); % Plot the imaginary wavefunction
 xlabel('$x\ (\AA)$', 'Interpreter','latex'); % Label the x-axis
 ylabel('$\mathrm{Im}(\psi(x, t))$', 'Interpreter','latex'); % Label the y-axis
+ylim([min(imag(psi_t(:))) max(imag(psi_t(:)))]); % Set the y-limits foe convenience
 title('Imaginary Component of the Wavefunction') % Add a title
 grid on; % Add a grid to the plot
 
@@ -94,6 +96,7 @@ subplot(1, 3, 3) % Right subfigure
 prob_density = plot(x_ang, abs(psi_t(:, 1)).^2); % Plot the initial probability density
 xlabel('$x\ (\AA)$', 'Interpreter','latex'); % Label the x-axis
 ylabel('$|\psi(x, t)|^2$', 'Interpreter','latex'); % Label the y-axis
+ylim([min(abs(psi_t(:)).^2) max(real(abs(psi_t(:)).^2))]); % Set the y-limits foe convenience
 title('Probability Density') % Add a title
 grid on; % Add a grid to the plot
 
