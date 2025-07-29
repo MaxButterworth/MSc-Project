7cccb30ba234a1686683c2fa4008760ac62a7cb1
MaxButterworth <126078250+MaxButterworth@users.noreply.github.com>
2025-07-29 11:51:01 +0100

    Specify the units of t_delay in the relevant comment.

--- Part_II_Free_Wave_Packet_Propagation/Interacting_Gaussian_Wave_Packets_Integral_Sim.m
+++ Part_II_Free_Wave_Packet_Propagation/Interacting_Gaussian_Wave_Packets_Integral_Sim.m
@@ -31,7 +31,7 @@ sigma_A = L/50; % Set the initial width of wave packet A
 x0_B = L/4; % Set the starting position of wave packet B on the x-axis
 k0_B = 10; % Set the expectation value for k for wave packet B
 sigma_B = L/50; % Set the initial width of wave packet B
-t_delay = 0; % Set the time delay from when wave packet B should be introduced into the system
+t_delay = 0; % Set the time delay (in units of dt) to specify when wave packet B should be introduced into the system
 
 % Determine whether periodic boundary conditions are activated or not
 set_PBC = false;
