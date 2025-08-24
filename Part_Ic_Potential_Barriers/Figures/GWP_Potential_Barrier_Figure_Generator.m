287b99e9e91ffffebbe523dc3765364e1c6a5676
MaxButterworth <126078250+MaxButterworth@users.noreply.github.com>
2025-08-25 00:49:49 +0100

    Update to SO prop method.

--- Part_Ic_Potential_Barriers/Figures/GWP_Potential_Barrier_Figure_Generator.m
+++ Part_Ic_Potential_Barriers/Figures/GWP_Potential_Barrier_Figure_Generator.m
@@ -3,6 +3,7 @@
 % ======================================================================================================================================
 
 % Part Ic - Free Particle Gaussian Wave Packet Incident on a Potential Barrier Simulation
+% Wave packet propagation is performed using the split operator method
 
 % Author: Max L Butterworth
 % MSc in Theoretical and Computational Chemistry Project
@@ -17,22 +18,38 @@ L = 100; % Length of the 1D box
 m = 1; % Mass of electron
 h = 1; % Planck's constant
 hbar = 1; % Definition of h bar
-N_steps = 1000; % Number of discretisation points for the x-axis
+N_steps = 1000; % Number of discretisation points
 
-wp_energy = 1; % Set the wave packet energy
-barrier_energy = 10; % Set the magnitude of the potential barrier height
-barrier_width = 20; % Set the barrier width in units of dx
+wp_energy = 50; % Set the wave packet energy
+barrier_energy = 20; % Set the magnitude of the potential barrier height
+barrier_width = 25; % Set the barrier width in units of dx
 
-k = sqrt((wp_energy * 2 * m)/(hbar^2)); % Calculate the wavenumber from wp_energy; k = 0 gives a stationary Gaussian wave packet
+x0 = 20; % Set the starting position of wave packet on the x-axis
+k0 = sqrt((wp_energy * 2 * m)/(hbar^2)); % Calculate the wavenumber from wp_energy; k = 0 gives a stationary Gaussian wave packet
+sigma = L/100; % Set the initial width of the wave packet
+
+include_elapsed_time = false; % Define a variable to show elapsed time on figure or not
+
+save_figures = false; % Define a variable to save figures at various points in the simulation or not
 
 % ======================================================================================================================================
-%%%%%%%%%% Discretise the spatial domain, x, and time domain, t %%%%%%%%%%
+%%%%%%%%%% Discretise the spatial domain, x; time domain, t; and k-space domain, k %%%%%%%%%%
 % ======================================================================================================================================
 
-x = linspace(0, L, N_steps); % Define the domain of the infinite potential well
+x = linspace(0, L, N_steps); % Define the x-domain
 dx = x(2) - x(1); % Calculate the spatial step size
 
-dt = 1e-1; % Define the time step size
+dk = (2 * pi)/L; % Define spacing in k-space
+
+if rem(N_steps, 2) == 0 % Define k-space grid if N_steps is even
+    k = dk * (-(N_steps/2):((N_steps/2)-1)).';
+
+else % Define k-space grid if N_steps is odd
+    k = dk * (-((N_steps-1)/2):((N_steps-1)/2)).';
+
+end
+
+dt = 1e-2; % Define the time step size
 N_t = 1000; % Define the number of time steps to simulate
 
 % ======================================================================================================================================
@@ -65,33 +82,53 @@ V_matrix = diag(V_vector); % Define a diagonal potential matrix
 H = (-((hbar^2)/(2*m)) * laplacian) + V_matrix; % Define the Hamiltonian operator
 
 % ======================================================================================================================================
-%%%%%%%%%% Generate an initial wave packet composed of one plane wave modulated by a Gaussian %%%%%%%%%%
+%%%%%%%%%% Generate an initial wave packet by applying the inverse Fourier transform to a Gaussian %%%%%%%%%%
 % ======================================================================================================================================
 
-x0 = L/4; % Start evolving the wave packet from the centre of the box at t = 0
-sigma = L/50; % Set the initial width of the wave packet
-psi0 = exp(-(x - x0).^2/(2 * sigma^2)) .* exp(1i * k * x); % Define the initial Gaussian wave packet
+a_0 = 1; % Prefactor for the Gaussian distribution
+phase = 0; % Define the phase term in the Gaussian distribution
+phase_coeff = 0; % Define the coefficients of the components of the phase term
+
+% Construct the Gaussian phase term in k-space
+for index = 1:length(phase_coeff)
+    phase = phase + (phase_coeff(index) * (k - k0).^(index - 1));
+end
+
+a_k = a_0 * exp((-(1/(2 * sigma^2)) * (k - k0).^2) + (1i * phase)); % Construct the whole Gaussian distribution in k-space
+psi0 = ifft(ifftshift(a_k .* exp(-1i * k * x0))); % Initial Gaussian wave packet in real space
+
 psi0_norm = psi0/sqrt(trapz(x, abs(psi0).^2)); % Normalise the initial Gaussian wave packet
 
 % ======================================================================================================================================
 %%%%%%%%%% Implement the Crank-Nicolson method to evolve the wavefunction and calculate probability current %%%%%%%%%%
 % ======================================================================================================================================
 
-J = zeros(N_steps, N_t); % Initialise an array to store probability currents
-first_deriv = spdiags([-1, 1], 0:1, N_steps, N_steps)/dx;
+% Define kinetic and potential operators
+p = hbar * k; % Calcualte the momentum at each point in k-space
 
-psi = psi0_norm(1:N_steps).'; % Set the initial value of the wavefunction
-psi_t = zeros(N_steps, N_t); % Initialise an array to store the wavefunction as it evolves in time
-psi_t(:, 1) = psi; % Store the initial wavefunction in the time evolution array
+T_op = exp(-(1i * (p.^2) * dt)/(2 * m * hbar)); % Kinetic energy operator (full time step)
+V_op = exp(-(1i * V_vector * dt)/(2 * hbar)); % Potential energy operator (half time step)
 
-% Pre-compute matrices required for the Crank-Nicolson method
-A = eye(N_steps) + (((1i * dt)/(2 * hbar)) * H);
-B = eye(N_steps) - (((1i * dt)/(2 * hbar)) * H);
+% Initialise arrays to store fluxes and a first derivative operator to calculate the fluxes
+J = zeros(N_steps, N_t); % Array to store probability currents
+first_deriv = spdiags([-1, 1], 0:1, N_steps, N_steps)/dx; % A matrix to calculte the first derivative using the finite difference method
 
-for t = 2:N_t % Loop over all time steps
-    psi = A \ (B * psi); % Evolve the wavefunction over time
-    psi = psi/sqrt(trapz(x, abs(psi).^2)); % Normalise the time-evolved wavefunction
-    psi_t(:, t) = psi; % Store the time-evolved wavefunction in the time evolution array
+% Set up the wave packet for propagation
+psi = psi0_norm(1:N_steps); % Set the initial value of the wave packet
+psi_t = zeros(N_steps, N_t); % Initialise an array to store the wave packet as it evolves in time
+psi_t(:, 1) = psi; % Store the initial wave packet in the time evolution array
+
+% Propagate the wave packet
+for t = 2:N_t
+    psi = V_op .* psi; % Operate a half time step in real space
+    psi_k = fftshift(fft(psi)); % Fourier transform the wave packet into k-space
+    psi_k = T_op .* psi_k; % Operate a full time step in k-space
+    psi = ifft(ifftshift(psi_k)); % Inverse Fourier transform into real space
+    psi = V_op .* psi; % Operate a half time step in real space
+
+    psi = psi/sqrt(trapz(x, abs(psi).^2)); % Normalise the time-evolved wave packet
+
+    psi_t(:, t) = psi; % Store the time-evolved wave packet in the time evolution array
     J(:, t) = -((1i * hbar)/(2 * m)) * ((conj(psi) .* (first_deriv * psi)) - ((first_deriv * conj(psi)) .* psi)); % Calculate probability current at each point along x
 end
 
@@ -155,6 +192,6 @@ for n = 1:N_t % Loop over all timesteps
     set(imag_wavefunction, 'YData', imag(psi_t(:, n))) % Update the imaginary part of the wavefunction
     set(prob_density, 'YData', abs(psi_t(:, n)).^2); % Update the probability density
     set(flux_plot, 'YData', J(:, n)); % Update the probability density
-    pause(0.1); % Pause to create an animation effect
+    pause(0.05); % Pause to create an animation effect
     drawnow; % Update the relevant figures
 end
